092fe0391f4052e82455c0fa86dec885f94b1589
MD.Razeen Kibria <razeenkibriaofficial@gmail.com>
2024-09-22 16:24:12 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,76 @@
+/*console.log("Hello Razeen !");
+
+var shoesPrice = 50;
+console.log(shoesPrice);
+
+let bikePrice = "5 lack";
+console.log(bikePrice);
+
+var myName = "MD.Razeen Kibria"; //keyword=( var ), maximum keyword was smaller letter used in js !
+console.log(myName);
+console.log(typeof myName);
+
+var isGood = true; // variable name like:( isGood ) , But you don't used any quotation like: ' '," " .                //Variable also Case-sensitive ..
+console.log(isGood);
+
+let isGoodStudent = false;
+console.log(isGoodStudent);
+console.log(typeof isGoodStudent);
+
+var id='I will promise I work hard as my limitation ';
+console.log(id.toUpperCase());
+console.log(id.toLowerCase());
+console.log(id.indexOf("will"));
+console.log(id.split('I'));
+
+var jobPaisos = true;
+var savingAmount = 5000;
+if ((jobPaisos = true && savingAmount > 20000)) {
+  console.log("Ali Tour e Jai Cholo");
+} else {
+  console.log("Tour Cancelled Ali & Razeen ");
+}
+
+var friendAge =[20,23,34,50];
+console.log(friendAge);
+
+var position =friendAge.length;
+console.log(position);
+
+friendAge.push(29);
+console.log(friendAge);
+
+friendAge.pop();
+console.log(friendAge);
+
+friendAge.shift();
+console.log(friendAge);
+
+var pos=friendAge.indexOf(23);
+console.log(pos);
+
+
+var friendName=['razeen','ratul','fahim','rakin','towkir'];
+var list=friendName.unshift('anika');
+console.log(friendName);
+
+var part =friendName.slice(2,6);
+console.log(part);
+
+*/
+
+var num=15;//declaration 
+while(num>=0){//condition statement
+  console.log(num);//print the values
+ num--;//increment or decrements the value
+}
+
+console.log("Next part :");
+
+var fullName=['MD','Razeen','Kibria','Ramim',];
+for(var i=0;i<fullName.length;i++)
+{
+  var allNames=fullName[i];
+  console.log(allNames);
+}
+
